ba66cad710168f11f1107b3c5dfba8e33d0a053e
Breanna <breannab121@gmail.com>
2025-07-03 21:11:33 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import IpTracker from './components/script/IpTracker';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+    <div>
+      <IpTracker />
+    </div>
+  );
 }
 
-export default App
+export default App;
